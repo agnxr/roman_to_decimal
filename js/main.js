ca7dcad9103adf0c5203e248bee90afee0b6d0b2
agnxr <ar21@o2.pl>
2018-02-04 15:06:38 +0100

    add converter

--- js/main.js
+++ js/main.js
@@ -1,30 +1,97 @@
+function convert() {
 
-document.addEventListener("DOMContentLoaded", function() {
+    var nr = document.querySelector("#number").value;
 
+    var romanToInt = function(s) {
 
-    var sentences = ["Po jednym spotkaniu, łatwo się zaprzyjaźnić, ale razem mieszkając, łatwo się pokłócić.", "Piękne słowa nie są wiarygodne, wiarygodne – nie są piękne.", "Bez odwagi nie zostaniesz dowódcą.", "Biorąc strój nowy, zapomnij o starym.", "Dobre lekarstwo jest gorzkie w ustach, ale skuteczne w chorobie.", "Dopiero ze szczytu góry można ocenić rozległość równiny.", "Gdy zbyt pełno w czarce – łatwo o przelanie.", "Góra Tai nie odrzuca najmniejszego pyłku i dlatego jest taka wysoka.", "Istnieje tylko dwóch dobrych ludzi – jeden zmarł, a drugi się jeszcze nie narodził.", "Jeśli się nikogo o nic nie prosi, wszyscy wydają się uczynni.", "Jeśli zdobędziesz się na cierpliwość w jednej chwili gniewu, zaoszczędzisz sobie sto dni przykrości." , "Jeżeli strzelec spudłuje, niechaj tylko w sobie samym szuka przyczyny swego niepowodzenia.", "Kto ma pieniądze, może zmusić diabła, by napędzał jego młyn.", "Kto ma w domu miliony, nie ma chwili spokoju.", "Kto nie myśli o jutrze, będzie miał kłopoty, zanim dziś się skończy.", "Kto otwiera swe serce dla ambicji – zamyka je przed spokojem.", "Kto pyta, jest głupcem przez pięć minut; kto nie pyta, pozostaje nim do końca.", "Kto szuka przyjaciela bez wad, pozostanie bez przyjaciela.", "Lód o grubości trzech stóp nie bierze się z jednego chłodnego dnia.", "Ludzie, chociaż nie dożyją nawet stu lat, to jednak stwarzają sobie troski na całe tysiąclecia.", "Mądra pszczoła nie pije ze zwiędłego kwiecia.", "Mówiąc tysiąc słów, trudno sobie nie zaszkodzić.", "Najwięcej czasu ma człowiek, który niczego na później nie odkłada.", "Najwięcej kłamie ten, kto najwięcej mówi o sobie.", "Nie kłóć się z głupim, bo postronni nie zauważą różnicy.", "Nie sprzedaje się drzewa w lesie ani ryby w jeziorze.", "Przeszłość, jeśli jej nie zapomnisz, stanie się drogowskazem w przyszłości.", "Przemycana sól jest smaczniejsza od sprzedawanej legalnie.", "Ten, kto przeniósł górę, zaczął od małych kamyków.", "Wiedza i doświadczenie przychodzą z wiekiem. Najczęściej jest to wieko od trumny.","Zamiast innych się radzić, radź się samego siebie.","Zanim zaczniesz poprawiać świat, przejdź się trzy razy przez swój własny dom.", "Żyj tak, jakbyś miał umrzeć jutro, a pracuj tak jakbyś miał przed sobą wieczność."];
+        var s = s.toUpperCase();
 
-
-
-
-    var button = document.querySelector('button');
-
+        var digits = (""+s).split("");
 
 
-    button.addEventListener("click", function(){
+        for (i=0; i<digits.length; i++){
 
-        var randomIndex = Math.floor(Math.random()*sentences.length);
-
-        var prediction = sentences[randomIndex];
-
-        var saying = document.querySelector("#saying");
-
-        saying.innerText = prediction;
+            if(digits[i] == "I"){
 
 
+                digits.splice(i,1, 1);
 
-    })
+            } else if (digits[i] == "V"){
 
-}); 
+                digits.splice(i, 1, 5);
 
+            } else if (digits[i] == "X"){
+
+                digits.splice(i, 1, 10);
+
+            } else if (digits[i] == "L"){
+
+                digits.splice(i, 1, 50);
+
+            } else if (digits[i] == "C"){
+
+                digits.splice(i, 1, 100);
+
+            } else if (digits[i] == "D"){
+
+                digits.splice(i, 1, 500);
+
+            } else if (digits[i] == "M"){
+
+                digits.splice(i, 1, 1000);
+
+            }
+
+        }
+
+
+        digits.reverse();
+
+
+
+        console.log(digits);
+
+
+
+        for (i=0; i<digits.length; i++) {
+
+            if (digits[i] === digits[i+1]) {
+
+                digits[i] = digits[i] + digits[i + 1];
+
+                digits.splice(i+1,1);
+
+            } else if (digits[i + 1] < digits[i]){
+
+                digits[i] =digits[i] - digits[i+1];
+
+                digits.splice(i + 1, 1);
+
+            } else if (digits[i + 1] < digits[i]) {
+
+                digits[i] = digits[i] + digits[i+1];
+
+                digits.splice(i+1,1);
+
+            }
+
+        }
+
+        var wynik=0;
+
+        for (i=0; i<digits.length; i++){
+
+            wynik += digits[i];
+
+        }
+
+
+        return wynik;
+
+    };
+
+
+    document.getElementById("converted").innerHTML = romanToInt(nr);
+
+}
 
