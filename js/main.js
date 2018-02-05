0c51623d20cd78e85dcbfa04f9fcd7dcb51147e4
agnxr <ar21@o2.pl>
2018-02-05 11:13:38 +0100

    add int to roman

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
+var input = document.querySelector("#number");
+var msg = document.querySelector("#message");
+
+input.onfocus = function (){
+    msg.style.display = 'block';
+};
+
+input.onblur = function() {
+    msg.style.display='none';
+};
+
+
 function convert() {
 
     var nr = document.querySelector("#number").value;
 
     var romanToInt = function(s) {
+
+        if (s == null || s == "") {
+            return "Wpisz jakąś liczbę";
+        }
 
         var s = s.toUpperCase();
 
@@ -40,18 +56,13 @@ function convert() {
 
                 digits.splice(i, 1, 1000);
 
+            } else {
+                return "Podaj poprawną liczbę";
             }
 
         }
 
-
         digits.reverse();
-
-
-
-        console.log(digits);
-
-
 
         for (i=0; i<digits.length; i++) {
 
@@ -95,3 +106,41 @@ function convert() {
 
 }
 
+
+function convert2() {
+
+    var ar = document.querySelector("#arabic").value;
+
+
+    var intToRoman = function(n) {
+
+        var n = parseInt(n);
+
+        if (n == null || n == "") {
+            return "Pole nie może być puste";
+        }
+
+        var regex=/^[0-9]+$/;
+
+        if (!ar.match(regex)) {
+
+            return "Liczba może składać się tylko z cyfr arabskich (0-9)";
+        }
+
+        var decimal = [1,   4,   5,   9,  10,  40,  50,  90, 100, 400, 500, 900,1000];
+        var roman = ["I","IV", "V","IX", "X","XL", "L","XC", "C","CD", "D","CM", "M"];
+
+        var res = "";
+
+        for (var i = 12; i >= 0; i--) {
+            while (n >= decimal[i]) {
+                res += roman[i];
+                n -= decimal[i];
+            }
+        }
+        return res;
+    };
+
+    document.getElementById("converted2").innerHTML = intToRoman(ar);
+
+}
